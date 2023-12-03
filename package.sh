e1465c9bde48f1f62f142fd3111b453d508ef151
rhubarb-geek-nz <rhubarb-geek-nz@users.sourceforge.net>
2023-12-03 23:21:32 +0000

    Changed to MIT license

--- package.sh
+++ package.sh
@@ -1,24 +1,6 @@
 #!/bin/sh -e
-#
-#  Copyright 2022, Roger Brown
-#
-#  This file is part of rhubarb pi.
-#
-#  This program is free software: you can redistribute it and/or modify it
-#  under the terms of the GNU General Public License as published by the
-#  Free Software Foundation, either version 3 of the License, or (at your
-#  option) any later version.
-# 
-#  This program is distributed in the hope that it will be useful, but WITHOUT
-#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-#  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-#  more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>
-#
-# $Id: package.sh 199 2022-09-29 23:48:06Z rhubarb-geek-nz $
-#
+# Copyright (c) 2023 Roger Brown.
+# Licensed under the MIT License.
 
 cleanup()
 {
@@ -136,7 +118,7 @@ Name: $PKGNAME
 Version: $VERSION
 Release: $RELEASE
 Group: Applications/System
-License: GPL
+License: MIT
 Requires: $REQUIRES
 BuildArch: noarch
 Prefix: /
